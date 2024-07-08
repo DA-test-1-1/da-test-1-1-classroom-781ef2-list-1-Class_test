21d094ee1f431841c0bfa0334de35b8817392a0f
dmorton714 <102087408+dmorton714@users.noreply.github.com>
2024-07-08 09:08:39 -0400

    seeing if this will grade

--- list1.py
+++ list1.py
@@ -23,7 +23,12 @@ from test import test
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  count = 0
+  for word in words:
+    if len(word) >= 2 and word[0] == word[-1]:
+      count = count + 1
+  return count
 
 
 # B. front_x
